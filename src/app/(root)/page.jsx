ffae2b43cb6ac4bcba1767a6ff0f26088bf93425
ssb-shree <bavachikarshree1666@gmail.com>
2025-12-11 16:27:02 +0530

    few opt in dome and lenis

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -208,7 +208,7 @@ const OjusCommonPage = () => {
         </section>
 
         {/* --------- --------- SECTION FOR MOBILE VIEW ONLY ------------------ */}
-        <section className="w-full h-[70vh] md:hidden flex flex-col gap-y-4 justify-center items-center p-6 bg-linear-to-b from-zinc-200 to-zinc-black text-black">
+        <div className="w-full h-[70vh] md:hidden flex flex-col gap-y-4 justify-center items-center p-6 bg-linear-to-b from-zinc-200 to-zinc-black text-black">
           <h3 className={` text-xl text-center ${alfa.className}`}>Experience The Radiance of Ojus</h3>
           <ImageMouseTrail
             className={"h-full"}
@@ -218,7 +218,7 @@ const OjusCommonPage = () => {
             imgClass="sm:w-40 w-28 sm:h-48 h-36 rounded-sm"
           ></ImageMouseTrail>
           <h3 className={` text-xl text-center font-light ${alfa.className} text-zinc-300`}>Swipe Or Tap Above</h3>
-        </section>
+        </div>
         {/* --------- --------- SECTION 3 ------------------ */}
         <section className="w-full md:mmin-h-screen flex justify-center items-center px-6 sm:px-12 md:px-20 py-16 bg-black text-white">
           <div className="hidden md:flex flex-col size-full md:mr-10">
@@ -262,67 +262,65 @@ const OjusCommonPage = () => {
         </section>
 
         {/* --------- --------- SECTION 4 ------------------ */}
-        <section id="highlights" className="w-screen h-[75vh] md:h-[80vh]">
-          <DomeGallery images={domeImages} grayscale={false} fitBasis={"max"} minRadius={1100} segments={30} />
+        <section id="highlights" className="w-screen h-[70vh] md:h-[80vh]">
+          <DomeGallery images={domeImages} grayscale={false} fitBasis={"max"} minRadius={1100} segments={40} />
         </section>
 
         {/* --------- --------- SECTION 5 ------------------ */}
-        <section className="w-screen">
-          <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
-            <aside>
-              <Link href="/" className="cursor-none h-20 flex items-center">
-                <img
-                  src="/apsit-logo-color.png"
-                  alt="apsit logo"
-                  className="h-full scale-75 sm:scale-90 md:scale-100 object-contain"
-                />
-              </Link>
-              <p>
-                A.P. Shah Institue Of Technology
-                <br />
-                Thane, Maharashtra
-              </p>
-            </aside>
-            <nav>
-              <h6 className="footer-title">Social</h6>
-              <div className="grid grid-flow-col gap-4">
-                <a>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    className="fill-current"
-                  >
-                    <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-                  </svg>
-                </a>
-                <a>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    className="fill-current"
-                  >
-                    <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-                  </svg>
-                </a>
-                <a>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    className="fill-current"
-                  >
-                    <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-                  </svg>
-                </a>
-              </div>
-            </nav>
-          </footer>
-        </section>
+        <footer className="footer w-screen sm:footer-horizontal bg-neutral text-neutral-content">
+          <aside>
+            <Link href="/" className="cursor-none h-20 flex items-center">
+              <img
+                src="/apsit-logo-color.png"
+                alt="apsit logo"
+                className="h-full scale-75 sm:scale-90 md:scale-100 object-contain"
+              />
+            </Link>
+            <p>
+              A.P. Shah Institue Of Technology
+              <br />
+              Thane, Maharashtra
+            </p>
+          </aside>
+          <nav>
+            <h6 className="footer-title">Social</h6>
+            <div className="grid grid-flow-col gap-4">
+              <a>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  className="fill-current"
+                >
+                  <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+                </svg>
+              </a>
+              <a>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  className="fill-current"
+                >
+                  <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+                </svg>
+              </a>
+              <a>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  className="fill-current"
+                >
+                  <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+                </svg>
+              </a>
+            </div>
+          </nav>
+        </footer>
       </main>
     </AuthProvider>
   );
--- src/components/LenisProvider.jsx
+++ src/components/LenisProvider.jsx
@@ -6,7 +6,7 @@ import Lenis from "@studio-freight/lenis";
 export default function LenisProvider({ children }) {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 3,
+      duration: 5,
       smooth: true,
     });
 
