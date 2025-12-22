021845083cc1bcd24c35d279acbc25f1c0cde4de
ssb-shree <bavachikarshree1666@gmail.com>
2025-12-23 03:32:03 +0530

    clear search on day seitch

--- src/app/sports/outdoor/page.jsx
+++ src/app/sports/outdoor/page.jsx
@@ -101,7 +101,10 @@ export default function OutdoorSportsPage() {
             <button
               className={`w-full p-2 rounded-lg ${activeTab === key ? "bg-[#1e293b]" : ""}`}
               key={key}
-              onClick={() => setActiveTab(key)}
+              onClick={() => {
+                setActiveTab(key);
+                searchInput("");
+              }}
             >
               {displayValue}
             </button>
