1a1a66918a3edae9524d0f75c785e1965e761ad1
Aditya Singh <adityasingh27072746@gmail.com>
2025-12-24 05:03:01 +0530

    cloudflare url added

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = ["https://ojus-sport26.onrender.com/", "http://127.0.0.1:8000/", "https://34.180.73.149/"];
+const baseURL = ["https://ojus-sport26.onrender.com/", "http://127.0.0.1:8000/", "https://34.180.73.149/", "https://specialist-apart-chrome-metadata.trycloudflare.com/"];
 const axiosInstance = axios.create({
   baseURL: baseURL[2],
   headers: {
