c3f8746283550cd45febc52e9cca0d25303d160e
Shree Bavachikar <bavachikarshree1666@gmail.com>
2025-12-24 16:45:30 +0530

    removed day 1 2 4 tabs button

--- src/app/sports/outdoor/page.jsx
+++ src/app/sports/outdoor/page.jsx
@@ -21,10 +21,7 @@ const spaceGrotesk = Space_Grotesk({
 });
 
 const tabs = [
-  { key: "day1", displayValue: "Day 1" },
-  { key: "day2", displayValue: "Day 2" },
   { key: "day3", displayValue: "Day 3" },
-  { key: "day4", displayValue: "Day 4" },
   { key: "day5", displayValue: "Day 5" },
 ];
 
